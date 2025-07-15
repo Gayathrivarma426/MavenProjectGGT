dc8b29b24c1221aabe27bcbc39594591d5b07e23
Gayathrivarma426 <gayathrivarma426@gmail.com>
2025-07-15 08:14:22 +0530

    push2

--- src/test/java/mavenproject/PracticingGit.java
+++ src/test/java/mavenproject/PracticingGit.java
@@ -4,6 +4,8 @@ public class PracticingGit {
 
 	public static void main(String[] args) {
 		//hello
+		
+		//bye and it is push2
 		System.out.println("create a simple maven project ");
 		System.out.println("Practicing");
 
