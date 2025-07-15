46a95db0d1815f752a6980cae0b8953f9fc55fda
Gayathrivarma426 <gayathrivarma426@gmail.com>
2025-07-15 08:12:08 +0530

    Update PracticingGit.java

--- src/test/java/mavenproject/PracticingGit.java
+++ src/test/java/mavenproject/PracticingGit.java
@@ -3,7 +3,7 @@ package mavenproject;
 public class PracticingGit {
 
 	public static void main(String[] args) {
-		
+		//hello
 		System.out.println("create a simple maven project ");
 		System.out.println("Practicing");
 
