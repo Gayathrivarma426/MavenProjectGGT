079abbf8bb68d02a83a18cb4c6aee758919657a5
Gayathrivarma426 <gayathrivarma426@gmail.com>
2025-07-15 08:20:43 +0530

    branching

--- src/test/java/mavenproject/PracticingGit.java
+++ src/test/java/mavenproject/PracticingGit.java
@@ -4,7 +4,7 @@ public class PracticingGit {
 
 	public static void main(String[] args) {
 		//hello
-		
+		//branch is adding
 		//bye and it is push2
 		System.out.println("create a simple maven project ");
 		System.out.println("Practicing");
